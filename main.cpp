814c94bb46b66c40ba8832561c7ef27d0cb5c5ac
kamil <freshmultikk21@gmail.com>
2019-08-02 22:24:13 +0200

    next one

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+string login, haslo;
+
+int main()
+{
+    cout << "podaj login";
+    cin >> login;
+    cout<< "podaj haslo";
+    cin >>haslo;
+if((login=="kamilko")&&(haslo=="tak"))
+    {cout<<"login i haslo poprawne, witamy";}
+
+else
+{
+ cout  <<"nie udalo sie zalogowac";
+}
+
+
+
+
+
+    return 0;
+}
